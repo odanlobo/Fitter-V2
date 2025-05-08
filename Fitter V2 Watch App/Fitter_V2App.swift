34a89a62972f92aa9f5fa093dd824aa792b70d38
Daniel Lobo <danielslobo96@gmail.com>
2025-05-08 15:20:45 -0300

    Initial Commit

---
+++ Fitter V2 Watch App/Fitter_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  Fitter_V2App.swift
+//  Fitter V2 Watch App
+//
+//  Created by Daniel Lobo on 08/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Fitter_V2_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fitter V2/Fitter_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  Fitter_V2App.swift
+//  Fitter V2
+//
+//  Created by Daniel Lobo on 08/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Fitter_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
